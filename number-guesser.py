f0c40f3ae9ee9864276311a87bea4d0b99c7ef1e
antoniosjefferson <itsmehtone@gmail.com>
2022-11-21 18:06:00 -0800

    finishes number guesses game

--- number-guesser.py
+++ number-guesser.py
@@ -1,10 +1,43 @@
 import random
 
-r = random.randrange(11)
+# TESTING RANDOM RANGE METHOD
+# r = random.randrange(11)
 
-n = input("Please choose a number between 1 and 10 ")
+# n = input("Please choose a number between 1 and 10 ")
 
-if int(n) == r:
-    print("Good job!")
+# if int(n) == r:
+#     print("Good job!")
+# else:
+#     print("Wrong! The number was " + str(r) + ", please try again!")
+
+top_range = input("Please type a number between 1 and 10: ")
+
+if top_range.isdigit():
+    top_range = int(top_range)
+
+    if top_range <= 0:
+        print("Please type a number larger than 0 next time.")
+        quit()
 else:
-    print("Wrong! The number was " + str(r) + ", please try again!")
+    print("Please type a number next time.")
+    quit()
+
+random_number = random.randrange(0, top_range)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time.")
+        continue
+
+    if user_guess == random_number:
+        print("You got it in " + str(guesses) + " tries!")
+        break
+    elif user_guess > random_number:
+        print("Too high!")
+    else:
+        print("Too low!")
